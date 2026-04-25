ade83dcfb58cad32c3f5daaa59229e9c4e90c7a5
TomiBalconist <tomislav@balconity.com>
2026-04-25 18:31:08 +0200

    Adjust SSR

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,20 @@
+<script setup>
+
+useHead({
+  htmlAttrs: {
+    lang: 'hr'
+  },
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | In Statera` : 'In Statera';
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'theme-color', content: '#059669' }
+  ]
+})
+</script>
+
 <template>
   <UApp>
     <AppHeader/>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,4 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'hr'
-      },
-      titleTemplate: '%s | In Statera',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'theme-color', content: '#059669' },
-      ]
-    }
-  },
   colorMode: {
     preference: 'light',
     fallback: 'light'
